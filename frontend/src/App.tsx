0b4c4b79c2ae5276c460159813f6af3e167ea82f
rajatmakholiya <rajatmakholiya07@gmail.com>
2025-09-01 18:09:56 +0530

    removed oauth residue from frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import AuthCallback from './pages/AuthCallback';
 import Dashboard from './pages/Dashboard';
 
 const App: React.FC = () => {
@@ -11,7 +10,6 @@ const App: React.FC = () => {
             <Routes>
                 <Route path="/" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/auth/callback" element={<AuthCallback />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
